99a79e9ac1362b9eece0c0d62f38e07b217a53a4
내 이름 <anjyounl24@gmail.com>
2024-11-11 22:39:22 +0900

    2024_11_11

--- My project/Assets/Scripts/SliderController.cs
+++ My project/Assets/Scripts/SliderController.cs
@@ -10,14 +10,36 @@ public class SliderController : MonoBehaviour
     
     public TextMeshProUGUI textMeshProUGUI = null;
     private int maxslidervalue = 100;
-    
-    void Changevalue(int value)
+    public AudioSource bgm;
+    public Slider wholev;
+    public Slider charv;
+    public Slider sfxv;
+    public Slider bgmv;
+
+    public void Changevalue(int value)
     {
-        Slider slider=transform.GetComponent<Slider>();
-        slider.value = value * maxslidervalue;
-        textMeshProUGUI.text = value.ToString("0");
+        int localvalue = value * maxslidervalue;
+        textMeshProUGUI.text = localvalue.ToString("0");
+        wholev.value = localvalue;
     }
 
-    
-      
+
+
+    public void Start()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+        wholev =canvas.transform.GetChild(1).GetChild(2).GetComponent<Slider>();
+        charv=canvas.transform.GetChild(2).GetChild(2).GetComponent<Slider>();
+        sfxv=canvas.transform.GetChild(3).GetChild(2).GetComponent<Slider>();
+        bgmv=canvas.transform.GetChild(4).GetChild(2).GetComponent<Slider>();
+        bgm=GameObject.Find("BGM").GetComponent<AudioSource>();
+        //bgmv.value = bgm.volume;
+
+    }
+
+    public void Update()
+    {
+        
+    }
+
 }
